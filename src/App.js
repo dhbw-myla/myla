944bae02af6619433afdf8452be83f8225f77864
Sascha Görnert <developer@wirtschaftsinformatik.se>
2020-01-13 14:39:45 +0100

    implemented login

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from './component/home/home';
 import User from './component/users';
+import Login from './component/login/login';
 
 function App() {
   return (
@@ -12,6 +13,9 @@ function App() {
       </Route>
       <Route exact path="/users">
         <User />
+      </Route>
+      <Route exact path="/login">
+        <Login />
       </Route>
     </Router>
   );
---
+++ src/component/login/login.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+
+import './login.css'
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: { username: "Hallo", password: "1234" }
+        }
+    }
+    render() {
+        const { user } = this.state;
+
+        return (
+            <div className="content">
+
+                <div className="container">
+                    <form action="senden.html" id="person">
+                        <label className="h2" form="person">Sign Up</label>
+
+                        <br />
+
+                        <label htmlFor="username">Username</label>
+                        <input
+                            className="form-control"
+                            type="text" name="username"
+                            id="username"
+                            maxLength="30"
+                            defaultValue={user.username}
+                        />
+
+                        <br />
+
+                        <label htmlFor="mail">E-Mail</label>
+                        <input
+                            className="form-control"
+                            type="text"
+                            name="mail"
+                            id="mail"
+                            maxLength="50"
+                        />
+
+                        <br />
+
+                        <label htmlFor="password">Password</label>
+                        <input
+                            className="form-control"
+                            type="password"
+                            name="password"
+                            id="password"
+                            maxLength="50"
+                            defaultValue={user.password}
+                        />
+
+                        <br />
+
+                        <label htmlFor="password-repeat">Repeat Password</label>
+                        <input
+                            className="form-control"
+                            type="password"
+                            name="password-repeat"
+                            id="passwordWDH"
+                            maxLength="50"
+                        />
+
+                        <br />
+
+                        <button id="resetBtn" className="btn twoButtons pressButton" type="reset">Reset</button>
+                        <button
+                            className="btn twoButtons pressButton"
+                            type="submit">Submit</button>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Login;
