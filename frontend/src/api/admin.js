3b20147378c1398add1f6f3aba95e1d415026596
Sascha Görnert <developer@wirtschaftsinformatik.se>
2020-05-23 13:53:12 +0200

    fixed getting all user
    - changed GET to POST

--- frontend/src/api/admin.js
+++ frontend/src/api/admin.js
@@ -4,7 +4,7 @@ import { PATH_ALL_USERS, PATH_BASE_URL } from './constants';
 export async function getAllUsers(user) {
    try {
       const url = PATH_BASE_URL + PATH_ALL_USERS;
-      const response = await axiosHelper.get(url, 'getAllUsers');
+      const response = await axiosHelper.post(url, 'getAllUsers', user);
       return response.data;
    } catch (error) {
       console.log('error on getAllUsers', error);
