fc202cad64fbe1dd3bb54401f9717f7dfa3b9517
msandig <MartinF95@web.de>
2020-05-27 09:07:09 +0200

    Update startpage.js

--- frontend/src/components/Home/startpage.js
+++ frontend/src/components/Home/startpage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import DHBWTeaser from '../../assets/DHBWIntro.jpg';
 import './startpage.css';
-import Signup from '../SignUp/signup';
+import Signup from '../SignUp/SignUp';
 import Login from './MyLogin';
 import { withRouter } from 'react-router-dom';
 
