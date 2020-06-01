efc9850e8181f66ad14e2a0870a44e8fcd835782
Sascha <developer@wirtschaftsinformatik.se>
2020-06-01 04:48:18 +0200

    fix errors, warning
    Co-authored-by: Rene Fischer <microsoft@renefischer.org>

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -9,7 +9,7 @@ class Footer extends Component {
    }
    render() {
       return (
-         <MDBFooter color="grey darken-2" dark>
+         <MDBFooter color="grey darken-2" dark="true">
             <p className="footer-copyright mb-0 py-3 mh-5 text-center fixed-bottom">
                &copy; {new Date().getFullYear()} Sascha Görnert, Niko Lockenvitz, Rene Fischer, Martin Sandig, Erik Jansky
                {/*, Julian Rolle */}
