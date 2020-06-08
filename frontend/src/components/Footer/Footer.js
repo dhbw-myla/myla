f6dd43f7b1560a84bae401d7bfb524edb2d194af
Niko Lockenvitz <nl@nikolockenvitz.de>
2020-06-08 21:53:48 +0200

    J
    * add MIT License
    
    * doc: spelling of corporate design (w/o hyphen)
    
    * last fixes
    
    final
    
    Co-authored-by: Fene Rischer <microsoft@renefischer.org>
    Co-authored-by: ErikJanskii <erik.jansky@gmail.com>
    Co-authored-by: msandig <MartinF95@web.de>
    Co-authored-by: Julian Rolle <julian.rolle@sap.com>
    Co-authored-by: SGoernert <developer@wirtschaftsinformatik.se>
    
    * final final final
    
    Co-authored-by: Fene Rischer <microsoft@renefischer.org>
    Co-authored-by: ErikJanskii <erik.jansky@gmail.com>
    Co-authored-by: msandig <MartinF95@web.de>
    
    * remove saschas and eriks spaces muhahaha
    
    Co-authored-by: Fene Rischer <microsoft@renefischer.org>
    Co-authored-by: ErikJanskii <erik.jansky@gmail.com>
    Co-authored-by: msandig <MartinF95@web.de>

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -11,7 +11,7 @@ class Footer extends Component {
       return (
          <MDBFooter color="grey darken-2" dark="true">
             <p className="footer-copyright mb-0 py-3 mh-5 text-center fixed-bottom">
-               &copy; {new Date().getFullYear()} Sascha Görnert, Niko Lockenvitz, Rene Fischer, Martin Sandig, Erik Jansky, Julian Rolle
+               &copy; 2020 Sascha Görnert, Niko Lockenvitz, Martin Sandig, Rene Fischer, Erik Jansky
             </p>
          </MDBFooter>
       );
